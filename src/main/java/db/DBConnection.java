a4f202db648487abfafe1199852b5b8524ef7fd9
Ivan <ikon1998@mail.ru>
2021-01-24 19:14:35 +0300

    implements class for database working

---
+++ src/main/java/db/DBConnection.java
@@ -0,0 +1,43 @@
+package db;
+
+import java.sql.*;
+
+public class DBConnection {
+
+    private static final String URL = "";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
+
+
+    private static DBConnection instance;
+
+    private Connection connection;
+    private Statement statement;
+
+    public void connect() throws SQLException {
+        connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        statement = connection.createStatement();
+    }
+
+    public static DBConnection getInstance() {
+        if (instance == null) {
+            instance = new DBConnection();
+        }
+
+        return instance;
+    }
+
+    public Statement getStatement() {
+        return statement;
+    }
+
+    public PreparedStatement createPrepareStatement(String query) throws SQLException {
+        return connection.prepareStatement(query);
+    }
+
+    public ResultSet executeQuery(String query) throws SQLException {
+
+        return statement.executeQuery(query);
+
+    }
+}
